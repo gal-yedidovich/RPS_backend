4ab22b7efd6b1e0f8cecc07a4230ed5b5c20f399
Gal-Yedidovich <gal.yedidovich@gmail.com>
2018-07-06 23:31:26 +0300

    CHANGE  - passing name in CHAT handler

--- src/httpHandlers/lobby/ChatHandler.java
+++ src/httpHandlers/lobby/ChatHandler.java
@@ -2,6 +2,7 @@ package httpHandlers.lobby;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import core.UserManager;
 import httpHandlers.CommonHandler;
 import networking.Network;
 import org.json.JSONObject;
@@ -13,10 +14,14 @@ public class ChatHandler implements HttpHandler {
 			var reqJson = CommonHandler.readRequestJson(request);
 			var token = reqJson.getInt("token");
 			var msg = reqJson.getString("msg");
+			var time = reqJson.getLong("time");
+			var name = UserManager.instance.get(token).getName();
 
 			CommonHandler.resSuccess(request);
 			Network.Lobby.broadcast(token, new JSONObject()
 					.put("type", "msg")
+					.put("time", time)
+					.put("name", name)
 					.put("msg", msg).toString());
 		} catch (Exception e) {
 			e.printStackTrace();
